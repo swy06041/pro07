f947fcd8b3301c7661e0e84fed53217c8403bc6f
swy06041 <swy06041@naver.com>
2023-06-08 17:38:09 +0900

    06/08 수정테스트

--- src/main/java/sec02/ex02/MemberServlet.java
+++ src/main/java/sec02/ex02/MemberServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/member3")
+@WebServlet("/member3")	//asdsad
 public class MemberServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
